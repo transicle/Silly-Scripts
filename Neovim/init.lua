0f4d43f5aac71689ecd11bd885615073a40d081a
lily <ilovesuno@proton.me>
2025-11-02 11:34:22 -0800

    Update init.lua

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -1,9 +1,24 @@
 -- General stuff
 
+vim.o.laststatus = 0
+vim.o.ruler = false
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 vim.opt.termguicolors = true
+
+-- Neovide
+
+vim.g.neovide_padding_top = 20
+vim.g.neovide_padding_bottom = 10
+vim.g.neovide_padding_left = 20
+vim.g.neovide_padding_right = 20
+vim.g.neovide_scale_factor = 0.85
+
+-- Install dependencies
+
+require("dependencies")
 
 -- Requiring ./lua files
 
@@ -18,9 +33,10 @@ local function require_files(directory, prefix)
 
         if stat then
             if stat.type == "file" and file:match("%.lua$") and (
-                file ~= "pacman.lua"    and
-                file ~= "binder.lua"    and
-                file ~= "dashboard.lua"
+                file ~= "pacman.lua"        and
+                file ~= "binder.lua"        and
+                file ~= "dashboard.lua"     and
+                file ~= "dependencies.lua"
             ) then
                 require(prefix .. file:gsub("%.lua$", ""))
             elseif stat.type == "directory" then
