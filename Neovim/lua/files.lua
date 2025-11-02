00297f11e69a05fbc7cf0bb51188e9c25e81ffa6
lily <ilovesuno@proton.me>
2025-11-02 11:34:57 -0800

    Update files.lua

--- Neovim/lua/files.lua
+++ Neovim/lua/files.lua
@@ -1,27 +1,45 @@
 local pacman = require("toolkits.pacman")
 local KeybindManager = require("toolkits.binder")
 
-pacman.use("https://github.com/nvim-lua/plenary.nvim")
+pacman.use("https://github.com/nvim-telescope/telescope.nvim", function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local telescope_builtin = require("telescope.builtin")
 
-pacman.use("https://github.com/ibhagwan/fzf-lua", function()
-    local fzf = require("fzf-lua")
-    fzf.setup({
-        winopts = {
-            height = 0.85,
-            width = 0.80,
-            row = 0.30,
-            col = 0.50,
+    telescope.setup({
+        defaults = {
+            layout_config = {
+                width = 0.85,
+                height = 0.85,
+                prompt_position = "top",
+            },
+            
+            sorting_strategy = "ascending",
+            borderchars = { "─", "│", "─", "│", "╭", "╮", "╯", "╰" },
+            mappings = {
+                i = {
+                    ["<esc>"] = actions.close,
+                    ["<C-h>"] = function(prompt_bufnr)
+                        actions.close(prompt_bufnr)
+                        telescope_builtin.find_files({ cwd = vim.fn.expand("~") })
+                    end,
+                },
+            },
         },
-        
-        fzf_opts = {
-            ["--layout"] = "reverse",
-            ["--info"] = "inline",
+
+        pickers = {
+            find_files = {
+                hidden = true,
+            },
         },
     })
 
     KeybindManager.map("n", "Alt + F", function()
-        fzf.files({
-            cwd = vim.fn.expand("%:p:h")
-        })
+        local cwd = vim.fn.expand("%:p:h")
+        if cwd == "" then
+            cwd = vim.loop.cwd()
+        end
+        
+        require("telescope.builtin").find_files({ cwd = cwd })
     end)
-end, "fzf-lua", "https://api.github.com/repositories/384850341/contents/lua/fzf-lua")
+end, "telescope", "https://api.github.com/repos/nvim-telescope/telescope.nvim/contents/lua/telescope")
