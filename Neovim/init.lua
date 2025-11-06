6aa81a516387086272c7b927de882d1098d5780d
lily <ilovesuno@proton.me>
2025-11-05 19:40:10 -0800

    Change Neovide settings

--- Neovim/init.lua
+++ Neovim/init.lua
@@ -3,26 +3,35 @@
 vim.opt.termguicolors = true
 
 if vim.g.neovide then
-    vim.g.neovide_scale_factor = 0.8
+    vim.g.neovide_scale_factor = 0.85
     vim.g.neovide_fullscreen = false
     vim.g.neovide_remember_window_size = true
     vim.g.neovide_hide_mouse_when_typing = true
+
     vim.g.neovide_cursor_vfx_mode = "railgun"
-    vim.g.neovide_cursor_vfx_particle_density = 5.0
-    vim.g.neovide_cursor_vfx_particle_lifetime = 1.2
-    vim.g.neovide_cursor_vfx_particle_speed = 10.0
-    vim.g.neovide_cursor_vfx_particle_phase = 1.0
-    vim.g.neovide_cursor_vfx_particle_curl = 1.0
-    vim.g.neovide_cursor_animation_length = 0.15 
-    vim.g.neovide_cursor_trail_size = 1.2
+    vim.g.neovide_cursor_vfx_particle_density = 15.0
+    vim.g.neovide_cursor_vfx_particle_lifetime = 1.0
+    vim.g.neovide_cursor_vfx_particle_speed = 25.0
+    vim.g.neovide_cursor_vfx_particle_phase = 1.8
+    vim.g.neovide_cursor_vfx_particle_curl = 3
+
+    vim.g.neovide_cursor_animation_length = 0.25
+    vim.g.neovide_cursor_trail_size = 2.0
     vim.g.neovide_cursor_antialiasing = true
+
     vim.g.neovide_scroll_animation_length = 0.3
-    vim.g.neovide_floating_blur_amount_x = 2.0
-    vim.g.neovide_floating_blur_amount_y = 2.0
-    vim.g.neovide_padding_top = 20
-    vim.g.neovide_padding_bottom = 20
-    vim.g.neovide_padding_right = 20
-    vim.g.neovide_padding_left = 20
+    vim.g.neovide_scroll_animation_far_lines = 0.6
+
+    vim.g.neovide_floating_blur_amount_x = 8.0
+    vim.g.neovide_floating_blur_amount_y = 8.0
+
+    vim.g.neovide_padding_top = 25
+    vim.g.neovide_padding_bottom = 25
+    vim.g.neovide_padding_right = 25
+    vim.g.neovide_padding_left = 25
+
+    vim.g.transparency = 0.85
+    vim.g.neovide_refresh_rate = 144
 end
 
 -- Load files
