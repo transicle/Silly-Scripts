49f1fd443b63285fc0bc36aff2f42b9363181ee4
lily <ilovesuno@proton.me>
2025-11-12 16:52:23 +0530

    Add more built in keybinds

--- Neovim/lua/plugins/dashboard.lua
+++ Neovim/lua/plugins/dashboard.lua
@@ -20,7 +20,9 @@ pacman.use("https://github.com/goolord/alpha-nvim", function()
         dashboard.button("Control + F, Alt + F", "  Find File", nil),
 	dashboard.button("Alt + .", "  Open Git TUI", nil),
 	dashboard.button("Alt + T, Control + Shift + T", "  Explorer", nil),
-        dashboard.button("Q", "  Quit", ":qa<CR>"),
+	dashboard.button("Control + I", "  Open Inline Chat", nil),
+	dashboard.button("R", "  Recent Files", ":Telescope oldfiles<CR>"),
+	dashboard.button("Q", "  Quit", ":qa<CR>"),
     }
 
     local loaded, failed, time = pacman.stats()
@@ -32,7 +34,7 @@ pacman.use("https://github.com/goolord/alpha-nvim", function()
     if height == 0 then
 	    height = 24
     end
-    
+ 
     local padding = math.floor((height - (#dashboard.section.header.val + #dashboard.section.buttons.val + #dashboard.section.footer.val + 8)) / 2)
 
     dashboard.config.layout = {
