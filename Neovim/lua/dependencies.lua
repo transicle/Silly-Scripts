5a59e3728c9eb806d2249654f8d4948c5d2dddf1
lily <ilovesuno@proton.me>
2025-11-12 16:51:03 +0530

    Remove useless dependencies

--- Neovim/lua/dependencies.lua
+++ Neovim/lua/dependencies.lua
@@ -11,14 +11,31 @@ pacman.use("https://github.com/nvim-lua/plenary.nvim", function() end, "plenary"
 
 pacman.use("https://github.com/neovim/nvim-lspconfig", function() end, "lspconfig")
 
-pacman.use("https://github.com/hrsh7th/nvim-cmp", function() end, "nvim-cmp")
-
-pacman.use("https://github.com/hrsh7th/cmp-nvim-lsp", function() end, "cmp-nvim-lsp")
-
 pacman.use("https://github.com/glepnir/lspsaga.nvim", function() end, "lspsaga")
-
-pacman.use("https://github.com/mfussenegger/nvim-dap", function() end, "nvim-dap")
 
 pacman.use("https://github.com/williamboman/mason.nvim", function() end, "mason")
 
 pacman.use("https://github.com/williamboman/mason-lspconfig.nvim", function() end, "mason-lspconfig")
+
+pacman.use("https://github.com/nvim-treesitter/nvim-treesitter", function()
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = {
+			"lua",
+			"typescript",
+			"javascript",
+			"python",
+			"rust",
+			"java",
+			"c",
+			"cpp"
+		},
+		highlight = {
+			enable = true
+		},
+		indent = {
+			enable = true
+		},
+	})
+end, "nvim-treesitter")
+
+pacman.use("https://github.com/nvim-tree/nvim-web-devicons", function() end, "nvim-web-devicons")
