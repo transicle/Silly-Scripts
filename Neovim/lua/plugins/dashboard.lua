3626fa7502c27e66fee9ff0fb17601a1f0378c4f
lily <ilovesuno@proton.me>
2025-11-12 16:54:36 +0530

    Update dashboard.lua

--- Neovim/lua/plugins/dashboard.lua
+++ Neovim/lua/plugins/dashboard.lua
@@ -18,16 +18,16 @@ pacman.use("https://github.com/goolord/alpha-nvim", function()
     dashboard.section.buttons.val = {
         dashboard.button("N", "  Make New", ":ene <CR>"),
         dashboard.button("Control + F, Alt + F", "  Find File", nil),
-	dashboard.button("Alt + .", "  Open Git TUI", nil),
-	dashboard.button("Alt + T, Control + Shift + T", "  Explorer", nil),
-	dashboard.button("Control + I", "  Open Inline Chat", nil),
-	dashboard.button("R", "  Recent Files", ":Telescope oldfiles<CR>"),
-	dashboard.button("Q", "  Quit", ":qa<CR>"),
+		dashboard.button("Alt + .", "  Open Git TUI", nil),
+		dashboard.button("Alt + T, Control + Shift + T", "  Explorer", nil),
+		dashboard.button("Control + I", "  Open Inline Chat", nil),
+		dashboard.button("R", "  Recent Files", ":Telescope oldfiles<CR>"),
+		dashboard.button("Q", "  Quit", ":qa<CR>"),
     }
 
     local loaded, failed, time = pacman.stats()
     dashboard.section.footer.val = {
-       string.format("Packages loaded: %d ⬩ Failed: %d ⬩ Loaded in %.2fs", loaded, failed, time)
+    	string.format("Packages loaded: %d ⬩ Failed: %d ⬩ Loaded in %.2fs", loaded, failed, time)
     }
 
     local height = vim.fn.winheight(0)
