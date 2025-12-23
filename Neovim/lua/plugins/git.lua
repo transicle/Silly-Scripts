b38b81901b244498782648d13943ff21655479dc
lily <ilovesuno@proton.me>
2025-12-23 04:54:00 -0500

    Delete Neovim/lua directory

--- Neovim/lua/plugins/git.lua
+++
@@ -1,8 +0,0 @@
-local pacman = require("core.pacman")
-local KeybindManager = require("core.binder")
-
-pacman.use("https://github.com/kdheepak/lazygit.nvim", function()
-	KeybindManager.map("n", "Alt + .", function()
-		vim.cmd("LazyGit")
-	end)
-end, "lazygit")
